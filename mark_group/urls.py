d9c026d0254ce8bd8d04a8af2bf3ac6cec7419d3
abhinavmabhi <dexmarketing192018@gmail.com>
2026-03-19 12:32:33 +0530

    first commit

---
+++ mark_group/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from app.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
